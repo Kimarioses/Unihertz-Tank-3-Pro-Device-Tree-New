d8320af64108e4318353be8bde3505ed0702788d
nino <pilopilosimon1990@gmail.com>
2026-01-14 17:18:50 +0100

    Create twrp_TANK3.mk

---
+++ twrp_TANK3.mk
@@ -0,0 +1,27 @@
+$(call inherit-product, device/oblue/TANK3/patches.mk ) 
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Configure twrp config common.mk
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device specific configs
+$(call inherit-product, device/oblue/TANK3/device.mk)
+
+# Configure launch_with_vendor_ramdisk.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Configure compression
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Configure virtual_ab_ota.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+PRODUCT_MANUFACTURER := A-gold
+PRODUCT_BRAND := Unihertz
+PRODUCT_MODEL := TANK3 Pro
